356e30bdc516144fd8e7ef7f76ed3d1ffd25bdcc
MinnanZhou <zhou-mn@hotmail.com>
2022-03-24 21:13:53 +0800

    update 22/3/24

--- 376.py
+++ 376.py
@@ -1,3 +1,14 @@
 class Solution:
     def wiggleMaxLength(self, nums) -> int:
-        pass
+        if len(nums) < 2:
+            return 1
+        count = 1
+        state = None
+        for index in range(1, len(nums)):
+            if nums[index] > nums[index-1] and state != 0:
+                count += 1
+                state = 0
+            elif nums[index] < nums[index-1] and state != 1:
+                count += 1
+                state = 1
+        return count
