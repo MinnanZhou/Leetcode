dc258beb3ffa4668ddff1fbadbce7f876b525cbb
MinnanZhou <zhou-mn@hotmail.com>
2022-04-06 01:38:35 +0800

    update 2022/4/6

--- 493.py
+++ 493.py
@@ -1,3 +1,16 @@
+import bisect
+
+
 class Solution:
     def reversePairs(self, nums) -> int:
-        st=[]
+        st = []
+        count = 0
+        for num in nums:
+            count += len(st) - bisect.bisect_right(st, num * 2)
+            bisect.insort_right(st, num)
+        return count
+
+
+a = Solution()
+inp = [5, 2, 3, 2, 3, 1]
+print(a.reversePairs(inp))
