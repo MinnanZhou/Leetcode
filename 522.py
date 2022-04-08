4a289ab05e465c275917f1e8acea319f08887583
MinnanZhou <zhou-mn@hotmail.com>
2022-04-09 01:13:42 +0800

    update 2022/4/9

---
+++ 522.py
@@ -0,0 +1,15 @@
+﻿class Solution:
+    def findLUSlength(self, strs) -> int:
+        strs = sorted(strs, key=lambda x: -len(x))
+
+        def check(target, compare):
+            return all(t in compare for t in target)
+
+        for i, word in enumerate(strs):
+            if all(not check(word, strs[j]) for j in range(len(strs)) if j != i):
+                return len(word)
+        return -1
+
+
+a=Solution()
+print(a.findLUSlength(["aabbcc", "aabbcc","c"]))
---
+++ 523.py
@@ -0,0 +1,23 @@
+﻿class Solution:
+    def checkSubarraySum(self, nums, k: int) -> bool:
+        rem = []
+        past = set()
+        past.add(0)
+        for i, num in enumerate(nums):
+            if num % k != 0:
+                rem.append(num % k)
+            elif i < len(nums) and nums[i + 1] % k == 0:
+                return True
+        last_rem = 0
+        for r in rem:
+            last_rem = (last_rem + r) % k
+            if last_rem in past:
+                return True
+            past.add(last_rem)
+        return False
+
+
+a = Solution()
+inp = [23, 2, 4, 6, 6]
+inp2 = 7
+print(a.checkSubarraySum(inp, inp2))
