4dc8efa8449014b4379632cfc247d31fb60e4fcb
MinnanZhou <zhou-mn@hotmail.com>
2022-04-17 00:00:05 +0800

    update 2022/4/16

---
+++ 538.py
@@ -0,0 +1,27 @@
+﻿class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def convertBST(self, root):
+        def func(tree, start):
+            if not tree:
+                return start
+            tree.val += func(tree.right, start)
+            left = func(tree.left, tree.val)
+            return left
+
+        func(root, 0)
+        return root
+
+
+a = Solution()
+inp = [TreeNode(4), TreeNode(1), TreeNode(6), TreeNode(0), TreeNode(2), TreeNode(5), TreeNode(7), None, None, None,
+       TreeNode(3), None, None, None, TreeNode(8)]
+for ii, x in enumerate(inp):
+    if x and ii * 2 + 1 < len(inp): x.left = inp[ii * 2 + 1]
+    if x and (ii + 1) * 2 < len(inp): x.right = inp[(ii + 1) * 2]
+print(a.convertBST(inp[0]))
--- 673.py
+++ 673.py
@@ -1,4 +1,35 @@
-﻿class Solution:
-    def findNumberOfLIS(self, nums) -> int:
-        count_set={}
+﻿import bisect
 
+
+class Solution:
+    def findNumberOfLIS(self, nums) -> int:
+        if not nums: return 0
+        n = len(nums)
+        length = [{} for _ in range(n + 1)]
+        length[0] = {float('-inf'): 1}
+        length[1] = {nums[0]: 1}
+        minRecord = [float('inf') for _ in range(n + 1)]
+        minRecord[1] = nums[0]
+        minRecord[0] = float('-inf')
+        recordList = [[] for _ in range(n + 1)]
+        recordList[1].append(nums[0])
+        recordList[0].append(float('-inf'))
+        for i in range(1, len(nums)):
+            pos1 = bisect.bisect_left(minRecord, nums[i]) - 1
+            pos2 = bisect.bisect_left(recordList[pos1], nums[i])
+            total = sum([length[pos1][recordList[pos1][j]] for j in range(pos2)])
+            if nums[i] in length[pos1 + 1]:
+                length[pos1 + 1][nums[i]] += total
+            else:
+                length[pos1 + 1][nums[i]] = total
+                bisect.insort_left(recordList[pos1 + 1], nums[i])
+            minRecord[pos1 + 1] = min(minRecord[pos1 + 1], nums[i])
+
+        k = -1
+        while not length[k]: k -= 1
+        return sum(length[k].values())
+
+
+a = Solution()
+inp = [2,2,2,2]
+print(a.findNumberOfLIS(inp))
