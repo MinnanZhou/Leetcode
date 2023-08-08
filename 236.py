2618bc3378cf71405f3258766f23eb8402b20ee0
Minnan Zhou <zhou-mn@hotmail.com>
2023-08-07 23:40:18 -0700

    update 8/7/23

--- 236.py
+++ 236.py
@@ -4,5 +4,25 @@ class TreeNode:
         self.left = None
         self.right = None
 
+
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root == p or root == q:
+            return root
+        left = right = None
+        if root.left:
+            left = self.lowestCommonAncestor(root.left, p, q)
+        if root.right:
+            right = self.lowestCommonAncestor(root.right, p, q)
+        if left and right:
+            return root
+        return left if left else right
+
+
+a = Solution()
+inp = [TreeNode(3), TreeNode(5), TreeNode(1), TreeNode(6), TreeNode(2), TreeNode(0), TreeNode(8), None, None,
+       TreeNode(7), TreeNode(4)]
+for ii, x in enumerate(inp):
+    if x and ii * 2 + 1 < len(inp): x.left = inp[ii * 2 + 1]
+    if x and (ii + 1) * 2 < len(inp): x.right = inp[(ii + 1) * 2]
+print(a.lowestCommonAncestor(inp[0], inp[-2], inp[-1]))
